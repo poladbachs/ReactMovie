9397e69dd219aa73fa4ea1e1a987ca3cbc13ffad
Polad <bakhip@usi.ch>
2024-09-28 03:01:21 +0200

    getting and logging movie data from API, refactored app.js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import { useEffect } from 'react';
 
 const API_URL = 'http://www.omdbapi.com?apikey=631507fc';
 
-const App = () => {
+export default function App() {
 
     const searchMovies = async (title) => {
         const res = await fetch(`${API_URL}&s=${title}`);
+        const data = await res.json();
+
+        console.log(data);
     }
 
     useEffect(() => {
-
+        searchMovies('Spiderman');
     }, [])
 
     return (
         <h1>App</h1>
     );
 }
-
-export default App;
