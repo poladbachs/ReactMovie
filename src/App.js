facf295e77920a920aa9bb5584d44e77005be063
Polad <bakhip@usi.ch>
2024-09-28 15:10:56 +0200

    movie container added, data is fetched and rendered

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import SearchIcon from './assets/search.svg'
 
 const API_URL = 'http://www.omdbapi.com?apikey=631507fc';
 
+const movie1 = {
+    "Title": "Batman Begins",
+    "Year": "2005",
+    "imdbID": "tt0372784",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+}
+
 export default function App() {
 
     const searchMovies = async (title) => {
@@ -34,6 +42,14 @@ export default function App() {
                     alt="search"
                     onClick={() => {}}
                 />
+            </div>
+
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
